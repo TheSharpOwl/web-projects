c0e70ccf75d0ca088dbf3298a2b31667184b03d3
SHADOW\ziadx <ziadxkabakibi@hotmail.com>
2025-06-24 09:44:26 +0400

    Add initial components and try with first name

---
+++ cv-builder/src/components/PersonalInfo.jsx
@@ -0,0 +1,13 @@
+import '../styles/common.css';
+
+function PersonalInfo() 
+{
+    return (
+    <div>
+        <h1 className="title-detail">First name</h1>
+    </div>
+    )
+}
+
+
+export default PersonalInfo
---
+++ cv-builder/src/components/SideEditor.jsx
@@ -0,0 +1,11 @@
+import PersonalInfo from "./PersonalInfo"
+
+function SideEditor() 
+{
+    // TODO add a selector between components here
+    return <PersonalInfo />
+}
+
+
+
+export default SideEditor 
